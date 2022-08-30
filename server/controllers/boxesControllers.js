6de577e2d6c642e2ba16dbcddacc11dbb53c8234
tahrer007 <tahrer_Ad@outlook.com>
2022-08-30 23:29:05 +0300

    adding new box

--- server/controllers/boxesControllers.js
+++ server/controllers/boxesControllers.js
@@ -4,15 +4,15 @@ const Box = require("../dataBase/models/boxes")
 const newBox =async (req, res) => {
   const newBox= new Box({
     type : req.body.type ,
-    amount : req.body.type ,
+    amount : req.body.amount ,
     usersId : req.body.usersId ,
     adminId : req.body.adminId ,
     addingHistory : req.body.addingHistory ,
 
   });
   try {
-    const addLocation = await newLocation.save();
-    res.status(201).json(addLocation);
+    const createBox = await newBox.save();
+    res.status(201).json(createBox);
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
