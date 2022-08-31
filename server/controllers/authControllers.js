5ef09c8c1d31df38eb98449ba2e667b4e0c6fbf8
tahrer007 <tahrer_Ad@outlook.com>
2022-09-01 00:57:14 +0300

    sign up function : hash the pasword & update the db

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,12 +1,46 @@
+const bcrypt = require("bcrypt");
+const User = require("../dataBase/models/users");
 
-const signUp =()=>{
-    
-    console.log("sign up :)")
+const signUp = async (req, res) => {
+ 
+  // hash the password
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hashedPassword) => {
+      // create a new user instance and collect the data
+      const user = new User({
+        email: req.body.email,
+        password: hashedPassword,
+      });
 
-}
+      // save the new user
+      user
+        .save()
+        // return success if the new user is added to the database successfully
+        .then((result) => {
+          res.status(201).send({
+            message: "User Created Successfully",
+            result,
+          });
+        })
+        // catch error if the new user wasn't added successfully to the database
+        .catch((error) => {
+          res.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+        });
+    })
+    // catch error if the password hash isn't successful
+    .catch((e) => {
+      res.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
+      });
+    });
+};
 
-const signIn =()=>{
-    console.log("sign in ^_^")
-
-}
-module.exports = {signUp,signIn};
+const signIn = () => {
+  console.log("sign in ^_^");
+};
+module.exports = { signUp, signIn };
