eda8fbe2bf879b76b21cdf719afbd210c5d86c64
tahrer007 <tahrer_Ad@outlook.com>
2022-09-07 16:20:42 +0300

    refactor signUp

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,46 +1,82 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../dataBase/models/users");
 
 const signUp = async (req, res) => {
- 
-  // hash the password
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hashedPassword) => {
-      // create a new user instance and collect the data
-      const user = new User({
-        email: req.body.email,
-        password: hashedPassword,
-      });
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(req.body.password, 10);
+  } catch (error) {
+    res.status(500).send({
+      message: "Password was not hashed successfully",
+      error,
+    });
+  }
+  const user = new User({
+    email: req.body.email,
+    password: hashedPassword,
+  });
 
-      // save the new user
-      user
-        .save()
-        // return success if the new user is added to the database successfully
-        .then((result) => {
-          res.status(201).send({
-            message: "User Created Successfully",
-            result,
+  try {
+    const result = await user.save();
+    res.status(201).send({
+      message: "User Created Successfully",
+      result,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: "Error creating user",
+      error,
+    });
+  }
+};
+
+const signIn = async (req, res) => {
+  /*User.findOne({ email: req.body.email })
+
+    .then((user) => {
+      bcrypt
+        .compare(req.body.password, user.password)
+
+        .then((passwordCheck) => {
+          console.log(passwordCheck);
+          if (!passwordCheck) {
+            return res.status(400).send({
+              message: "Passwords does not match",
+              error,
+            });
+          }
+
+          const token = jwt.sign(
+            {
+              userId: user._id,
+              userEmail: user.email,
+            },
+            "RANDOM-TOKEN",
+            { expiresIn: "24h" }
+          );
+
+          //   return success res
+          res.status(200).send({
+            message: "Login Successful",
+            email: user.email,
+            token,
           });
         })
-        // catch error if the new user wasn't added successfully to the database
+        // catch error if password does not match
         .catch((error) => {
-          res.status(500).send({
-            message: "Error creating user",
+          res.status(400).send({
+            message: "Passwords does not match",
             error,
           });
         });
     })
-    // catch error if the password hash isn't successful
+    // catch error if email does not exist
     .catch((e) => {
-      res.status(500).send({
-        message: "Password was not hashed successfully",
+      res.status(404).send({
+        message: "Email not found",
         e,
       });
-    });
-};
-
-const signIn = () => {
-  console.log("sign in ^_^");
+    });*/
 };
 module.exports = { signUp, signIn };
