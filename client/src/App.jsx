a7459ad54e6806f9a5b313079021d1a7a924487c
tahrer007 <tahrer_Ad@outlook.com>
2022-09-20 21:53:56 +0300

    set global sass , routers , basic app styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 
-import './App.scss';
+import './app.scss';
+import Home from './pages/home/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+<Route path="/" element={<Home />} />
 
 function App() {
   return (
-    <div className="App">
-      test
+    <div className="app">
+ 
+ <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+        
+  
     </div>
   );
 }
---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import React , {useState , useEffect} from "react";
+import "./home.scss" ;
+//1- render loadig 
+//2-check local sortage ; 
+// if has data ... check for update  else login page
+//if has data without inter net connection .. load latest data with offline message 
+//form contain auto log in 
+//
+
+const Home= ()=>{
+
+    const [loading,setLoading]=useState(true);
+
+    return( <div className="pageContainer homePage">home</div>)
+
+}
+
+export default Home ;
