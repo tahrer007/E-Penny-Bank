0808790167bbab6f6cbcdd986ae71e7c5c0f0134
tahrer007 <tahrer_Ad@outlook.com>
2022-09-22 01:30:15 +0300

    random deposit functions

--- client/src/components/deposit/randomDeposit/RandomDeposit.jsx
+++ client/src/components/deposit/randomDeposit/RandomDeposit.jsx
@@ -1,22 +1,28 @@
-import React , {useState , useEffect} from "react";
-import "./randomDeposit.scss" ;
+import React, { useState, useEffect } from "react";
+import { MAX,MIN } from "services/api/const";
 
-const RandomDeposit= ()=>{
+import "./randomDeposit.scss";
 
-    return(<div className="randomBox">
+const RandomDeposit = () => {
+  const [amount, setAmount] = useState(null);
+  const [min,setMin] = useState(MIN);
+  const [max,setMax]=useState(MAX);
 
-    <div className="leftSide">
-        <div className="slider">slider</div>
+  const randomPicker = (min, max) => {
+    console.log((Math.random() * (max - min) + min).toFixed(2));
+  };
+  return (
+    <div className="randomBox">
+      <div className="leftSide">
+        <div className="slider"></div>
         <div className="amount"> amount</div>
-
-    </div>
-    <div className="rightSide">
-        <div className="random">random</div>
+      </div>
+      <div className="rightSide">
+        <div className="random" onClick={()=>randomPicker(min,max)}>random</div>
         <div className="reveal">reveal </div>
+      </div>
     </div>
+  );
+};
 
-    </div>)
-
-}
-
-export default RandomDeposit ;
+export default RandomDeposit;
---
+++ client/src/services/api/const.js
@@ -0,0 +1,5 @@
+const MAX = 100 ; 
+const MIN = 1 ; 
+
+
+export {MAX,MIN};
