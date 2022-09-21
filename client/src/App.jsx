d8d1a1ffc56d680718727115ccba9b6cf3569aca
tahrer007 <tahrer_Ad@outlook.com>
2022-09-21 12:23:43 +0300

    nav bar , relative path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './app.scss';
 import Home from './pages/home/Home';
+import NavBar from './components/navbar/NavBar';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 <Route path="/" element={<Home />} />
 
---
+++ client/src/components/navbar/NavBar.jsx
@@ -0,0 +1,12 @@
+import "./navBar.scss";
+
+const NavBar = () => {
+  return (
+    <div className="navBar">
+      <div className="tilte">title</div>
+      <div className="icon"> icon</div>
+    </div>
+  );
+};
+
+export default NavBar;
