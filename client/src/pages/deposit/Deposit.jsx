7f26f84b18f67706c0dbddb6d25163158508080b
tahrer007 <tahrer_Ad@outlook.com>
2022-09-21 20:09:20 +0300

    deposit page

--- client/src/pages/deposit/Deposit.jsx
+++ client/src/pages/deposit/Deposit.jsx
@@ -1,10 +1,23 @@
-import React , {useState , useEffect} from "react";
-import "./deposit.scss" ;
+import React, { useState, useEffect } from "react";
+import "./deposit.scss";
 
-const Deposit= ()=>{
+const Deposit = () => {
+  const [randomSelected, setRandomSlected] = useState(true);
+  return (
+    <div className="pageContainer depositPage">
+      <div className="boxName">personal box</div>
+      <div className="optionsBox">
 
-    return(<div className="pageContainer DepositPage">deposit</div>)
+        options
+        {/*<div onChange={this.onChangeValue}>
+        <input type="radio" value="Random " name="gender" /> Male
+        <input type="radio" value="Female" name="gender" /> Female
+  </div>*/}
+      </div>
+      <div className="selectedOption">selectedOption</div>
+      <div className="submitBtn"> button</div>
+    </div>
+  );
+};
 
-}
-
-export default Deposit ;
+export default Deposit;
