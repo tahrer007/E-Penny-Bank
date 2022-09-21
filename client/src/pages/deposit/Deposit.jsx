a651e7f45bcddba3026f0cc29c5979ff450a10ac
tahrer007 <tahrer_Ad@outlook.com>
2022-09-21 20:57:06 +0300

    options box

--- client/src/pages/deposit/Deposit.jsx
+++ client/src/pages/deposit/Deposit.jsx
@@ -3,16 +3,19 @@ import "./deposit.scss";
 
 const Deposit = () => {
   const [randomSelected, setRandomSlected] = useState(true);
+  const onChangeSelection = (e) => setRandomSlected(!randomSelected);
+
+  useEffect(() => {
+    console.log(randomSelected);
+  }, [randomSelected]);
+
   return (
     <div className="pageContainer depositPage">
       <div className="boxName">personal box</div>
-      <div className="optionsBox">
-
-        options
-        {/*<div onChange={this.onChangeValue}>
-        <input type="radio" value="Random " name="gender" /> Male
-        <input type="radio" value="Female" name="gender" /> Female
-  </div>*/}
+      <div className="optionsBox" onChange={(e) => onChangeSelection(e)}>
+        <input className="radioBtn" type="radio" value={true} name="gender" defaultChecked={true} />{" "}
+        Random deposit
+        <input className="radioBtn" type="radio" value={false} name="gender" /> exact deposit
       </div>
       <div className="selectedOption">selectedOption</div>
       <div className="submitBtn"> button</div>
