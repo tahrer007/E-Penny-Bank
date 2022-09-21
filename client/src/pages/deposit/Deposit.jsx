d52bafd8acbca6f1e1fcd231de55df7858ef13cb
tahrer007 <tahrer_Ad@outlook.com>
2022-09-21 21:00:43 +0300

    selectedOption box

--- client/src/pages/deposit/Deposit.jsx
+++ client/src/pages/deposit/Deposit.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./deposit.scss";
-
+import RandomDeposit from "components/deposit/randomDeposit/RandomDeposit";
+import ExactDeposit from "components/deposit/exactDeposit/ExactDeposit";
 const Deposit = () => {
   const [randomSelected, setRandomSlected] = useState(true);
   const onChangeSelection = (e) => setRandomSlected(!randomSelected);
@@ -13,11 +14,20 @@ const Deposit = () => {
     <div className="pageContainer depositPage">
       <div className="boxName">personal box</div>
       <div className="optionsBox" onChange={(e) => onChangeSelection(e)}>
-        <input className="radioBtn" type="radio" value={true} name="gender" defaultChecked={true} />{" "}
+        <input
+          className="radioBtn"
+          type="radio"
+          value={true}
+          name="gender"
+          defaultChecked={true}
+        />
         Random deposit
-        <input className="radioBtn" type="radio" value={false} name="gender" /> exact deposit
+        <input className="radioBtn" type="radio" value={false} name="gender" />
+        exact deposit
       </div>
-      <div className="selectedOption">selectedOption</div>
+      <div className="selectedOption">
+        {randomSelected ? <RandomDeposit /> : <ExactDeposit />}
+      </div>
       <div className="submitBtn"> button</div>
     </div>
   );
