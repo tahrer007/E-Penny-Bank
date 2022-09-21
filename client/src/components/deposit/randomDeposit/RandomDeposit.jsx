5b0f468a5c5d29a3eb985f2329c24a07435e3f25
tahrer007 <tahrer_Ad@outlook.com>
2022-09-22 00:40:04 +0300

    random deposite

--- client/src/components/deposit/randomDeposit/RandomDeposit.jsx
+++ client/src/components/deposit/randomDeposit/RandomDeposit.jsx
@@ -3,7 +3,19 @@ import "./randomDeposit.scss" ;
 
 const RandomDeposit= ()=>{
 
-    return(<div className="">Random Deposit</div>)
+    return(<div className="randomBox">
+
+    <div className="leftSide">
+        <div className="slider">slider</div>
+        <div className="amount"> amount</div>
+
+    </div>
+    <div className="rightSide">
+        <div className="random">random</div>
+        <div className="reveal">reveal </div>
+    </div>
+
+    </div>)
 
 }
 
