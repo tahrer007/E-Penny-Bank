608f1d23d78e05512994fee56693e583d1fbb8e1
tahrer007 <tahrer_Ad@outlook.com>
2022-09-24 11:57:57 +0300

    main new page sections

--- client/src/pages/createBox/CreateBoxPage.jsx
+++ client/src/pages/createBox/CreateBoxPage.jsx
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from "react";
-import "./createBox.scss" ;
+import "./createBox.scss";
 const CreateBox = () => {
-  
-  
   useEffect(() => {
     console.log("test");
   }, []);
 
-  return (
-    <div className="pageContainer newBoxPage">
-      new box 
-      
+  return <div className="pageContainer newBoxPage">
+    <div className="boxDetails">
+    box Details 
     </div>
-  );
+    <div className="sharedBoxDetails"> shared Box Details</div>
+    <div className="createBtn"> createBtn</div>
+  
+  
+  
+  </div>;
 };
 
 export default CreateBox;
