2ef2d9752bf4a99907b3724856bc09c277e762cf
tahrer007 <tahrer_Ad@outlook.com>
2022-09-26 23:55:38 +0300

    spelling mistake

--- server/routes/boxesRouter.js
+++ server/routes/boxesRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const {newBox,getBoxes,deposit} = require("../controllers/boxesControllers");
 
-router.get('/allBoxes', getBoxs);
+router.get('/allBoxes', getBoxes);
 router.post('/newBox', newBox);
 router.post('/deposit', deposit);
 //router.post('/updateBox', updateBox);
