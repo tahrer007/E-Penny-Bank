cff456163aa8cfa5b8c1f79e6adf73c4f7da71b7
tahrer007 <tahrer_Ad@outlook.com>
2022-09-26 10:08:39 +0300

    new box : create box key and checkbox  autharized

--- client/src/components/sharedBoxDetails/SharedBoxDetails.jsx
+++ client/src/components/sharedBoxDetails/SharedBoxDetails.jsx
@@ -4,18 +4,44 @@ import "./sharedBoxDetails.scss";
 
 const SharedBoxDetails = ({ boxDetails, newBox }) => {
   const [boxKey, setBoxKey] = useState(boxDetails?.key);
+  const [isAllowedToReveal, setIsAllowedToReveal] = useState(
+    boxDetails?.isAllowedToReveal || false
+  );
+  const share = () => {
+    if (newBox) return;
+    console.log(boxKey);
+  };
 
   useEffect(() => {
-   if( newBox)setBoxKey(makeKey());
+    if (newBox) setBoxKey(makeKey());
   }, []);
+
+  useEffect(() => {
+    console.log(isAllowedToReveal);
+  }, [isAllowedToReveal]);
   return (
     <div className="sharedBoxDetails">
       <div className="keyBox">
         <div className="key">{boxKey}</div>
-        <div className="shareKey">sharedIcon</div>
+        {
+          //TODO:add unclickable style on share button}}
+        }
+        <div className="shareKey" onClick={share}>
+          shared icon
+        </div>
       </div>
       {(newBox || boxDetails.admin) && (
-        <div className="authorized">allow other users to reveal history</div>
+        <div className="authorized">
+          <div className="revealBox">
+            <input
+              type="checkbox"
+              checked={isAllowedToReveal}
+              value="allow"
+              onChange={() => setIsAllowedToReveal(!isAllowedToReveal)}
+            />
+            allow other users to reveal history
+          </div>
+        </div>
       )}
     </div>
   );
