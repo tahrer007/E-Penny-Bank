51d33e73e43d4306104a969405dea67c64cfa1d3
tahrer007 <tahrer_Ad@outlook.com>
2022-09-27 00:59:06 +0300

    box details main parts and styling

--- client/src/pages/boxDetails/BoxDetails.jsx
+++ client/src/pages/boxDetails/BoxDetails.jsx
@@ -3,6 +3,7 @@ import SharedBoxDetails from "components/sharedBoxDetails/SharedBoxDetails";
 import "./boxDetails.scss";
 
 const box = {
+  boxName: "vication",
   type: 1,
   totalDeposits: 0,
   usersId: ["6331f73f92d30d25c7103d29"],
@@ -10,9 +11,9 @@ const box = {
   depositsHistory: [{}],
   boxKey: "testtttt",
   isAllowedToReveal: true,
-  _id: "6331f7d692d30d25c7103d2d",
-  createdAt: "2022-09-26T19:04:54.725Z",
-  updatedAt: "2022-09-26T19:04:54.725Z",
+  _id: "63321e602105c96f0f3f57ed",
+  createdAt: "2022-09-26T21:49:20.156Z",
+  updatedAt: "2022-09-26T21:49:20.156Z",
   __v: 0,
 };
 const SavingBox = ({ boxId }) => {
@@ -20,21 +21,26 @@ const SavingBox = ({ boxId }) => {
     console.log(boxKey, isAllowedToReveal);
 
   return (
-    <div className="pageContainer box">
-      <div className="boxMainDetails">
-            <div className="name">{box.boxName}</div>
-            <div className="totalDeposit"></div>
-            <div className="date latestUpdate"></div>
-            <div className="date createdDate"></div>
-
+    <div className="pageContainer boxDetails">
+      <div className="MainDetails">
+        <div className="name">{box.boxName}</div>
+        <div className="totalDeposit"></div>
+        <div className="date latestUpdate"></div>
+        <div className="date createdDate"></div>
       </div>
       {box.type ? (
         <SharedBoxDetails
           newBox={false}
           getSharedBoxDetails={getSharedBoxDetails}
-          boxDetails ={box}
+          boxDetails={box}
         />
+        
       ) : null}
+      <div className="buttonsBox">
+            <div className="deposit">deposit</div>
+            <div className="history">history</div>
+            <div className="save">save</div>
+        </div>
     </div>
   );
 };
