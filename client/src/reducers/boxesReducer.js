849e85dc1e6e2a536af34f3d679b204fe60716b7
tahrer007 <tahrer_Ad@outlook.com>
2022-09-29 22:25:54 +0300

    selected box reducer

--- client/src/reducers/boxesReducer.js
+++ client/src/reducers/boxesReducer.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
 export const boxesReducer = (state = [], action) => {
-  console.log("Action", action.payload);
   switch (action.type) {
     case "FETCH_BOXES":
       return [...state, action.payload];
