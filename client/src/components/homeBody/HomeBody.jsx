1e13ebe914b713a7da000a02e2a6770a7fa647ef
tahrer007 <tahrer_Ad@outlook.com>
2022-10-14 22:00:50 +0300

    homebody

--- client/src/components/homeBody/HomeBody.jsx
+++ client/src/components/homeBody/HomeBody.jsx
@@ -3,7 +3,14 @@ import "./homeBody.scss";
 import { useSelector } from "react-redux";
 import { selectCurrentUser } from "features/auth/authSlice";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser ,faPiggyBank ,faPlus ,faSun ,faMoon ,faRightFromBracket} from "@fortawesome/free-solid-svg-icons";
+import {
+  faUser,
+  faPiggyBank,
+  faPlus,
+  faSun,
+  faMoon,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
 
 function HomeBody() {
   const user = useSelector(selectCurrentUser);
@@ -17,11 +24,21 @@ function HomeBody() {
       <div className="navButton columnFlex">
         <FontAwesomeIcon icon={faPiggyBank} />
         Boxes
-        </div>
-        
-      <div className="navButton columnFlex">NewBox</div>
-      <div className="navButton columnFlex">Theme</div>
-      <div className="navButton columnFlex">Log out</div>
+      </div>
+      <div className="navButton columnFlex">
+        <FontAwesomeIcon icon={faPlus} />
+        NewBox
+      </div>
+
+      <div className="navButton columnFlex">
+        <FontAwesomeIcon icon={faMoon} />
+        theme
+      </div>
+
+      <div className="navButton columnFlex">
+        <FontAwesomeIcon icon={faRightFromBracket} />
+        Log out
+      </div>
     </div>
   );
 }
