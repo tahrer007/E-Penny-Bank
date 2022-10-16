72710be70cd1874fa6e6ee198da8a3dc9a1b5e60
tahrer007 <tahrer_Ad@outlook.com>
2022-10-17 02:06:31 +0300

    theme color s

--- client/src/components/homeBody/HomeBody.jsx
+++ client/src/components/homeBody/HomeBody.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./homeBody.scss";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { selectCurrentUser } from "features/auth/authSlice";
+import { toggleDarkMode } from "features/them/themSlice";
+import { selectedCurrentMode } from "features/them/themSlice";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faUser,
@@ -15,7 +17,11 @@ import {
 
 function HomeBody() {
   const user = useSelector(selectCurrentUser);
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
+  const darkMode = useSelector(selectedCurrentMode);
+  const dispatch = useDispatch();
+  const handleCheck = () => dispatch(toggleDarkMode());
+  console.log(darkMode);
 
   return (
     <div className="innerButtonsWrapper homeMainBody">
@@ -23,22 +29,28 @@ function HomeBody() {
         <FontAwesomeIcon icon={faUser} />
         Profile
       </div>
-      <div className="mainBtns columnFlex" onClick={()=>navigate("../boxesList")}>
+      <div
+        className="mainBtns columnFlex"
+        onClick={() => navigate("../boxesList")}
+      >
         <FontAwesomeIcon icon={faPiggyBank} />
         Boxes
       </div>
-      <div className="mainBtns columnFlex" onClick={()=>navigate("../box/new")}>
-        <FontAwesomeIcon icon={faPlus}  />
+      <div
+        className="mainBtns columnFlex"
+        onClick={() => navigate("../box/new")}
+      >
+        <FontAwesomeIcon icon={faPlus} />
         NewBox
       </div>
 
-      <div className="mainBtns columnFlex">
-        <FontAwesomeIcon icon={faMoon} />
+      <div className="mainBtns columnFlex" onClick={handleCheck}>
+        <FontAwesomeIcon icon={darkMode ? faMoon : faSun} />
         theme
       </div>
-    <div></div>
+      <div></div>
       <div className="mainBtns columnFlex">
-        <FontAwesomeIcon icon={faRightFromBracket} />
+        <FontAwesomeIcon icon={faRightFromBracket} className="themeBtn" />
         Log out
       </div>
     </div>
--- client/src/components/pages/pubic/Public.jsx
+++ client/src/components/pages/pubic/Public.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+
 import "./public.scss";
 import SvgLogo from "components/Svg/SvgLogo";
 import { useNavigate } from "react-router-dom";
