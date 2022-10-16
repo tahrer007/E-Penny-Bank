56cf092fbe8e84f74f063830983f819dff2dade4
tahrer007 <tahrer_Ad@outlook.com>
2022-10-16 13:17:22 +0300

    random deposit , slider

--- client/src/components/deposit/randomDeposit/RandomDeposit.jsx
+++ client/src/components/deposit/randomDeposit/RandomDeposit.jsx
@@ -4,24 +4,37 @@ import Instructions from "components/reusables/form/instructions/Instructions";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoins, faEye } from "@fortawesome/free-solid-svg-icons";
 import "./randomDeposit.scss";
+import MultiRangeSlider from "components/reusables/multiRangeSlider/MultiRangeSlider";
 
 const RandomDeposit = ({ getValue }) => {
   const [value, setValue] = useState(null);
-  const [min, setMin] = useState(MIN);
-  const [max, setMax] = useState(MAX);
+  const [minVal, setMin] = useState(MIN);
+  const [maxVal, setMax] = useState(MAX);
 
   const randomPicker = () => {
-    const randomNum = (Math.random() * (max - min) + min).toFixed(2);
+    const randomNum = Number(
+      (Math.random() * (maxVal - minVal) + minVal).toFixed(2)
+    );
+
     setValue(randomNum);
     getValue(randomNum);
   };
-  const showAmount =()=> console.log(value) ; 
+
+  const handleRangeChange = ({ min, max }) => {
+    setMin(min);
+    setMax(max);
+  };
+  useEffect(() => {
+    console.log(minVal, maxVal);
+  }, [minVal, maxVal]);
+  const showAmount = () => console.log(value);
   return (
     <div className="randomWrapper">
-      <div className="slider">slider</div>
+      {<MultiRangeSlider min={MIN} max={MAX} onChange={handleRangeChange} />}
 
-      <div className="show" onClick={showAmount}>
-        <FontAwesomeIcon icon={faEye} /> Show Amount
+      <div className={`show ${!value && "disabled"}`} onClick={showAmount}>
+        <FontAwesomeIcon icon={faEye} />
+        Show Amount
       </div>
 
       <div className="random" onClick={randomPicker}>
@@ -32,4 +45,3 @@ const RandomDeposit = ({ getValue }) => {
 };
 
 export default RandomDeposit;
-
