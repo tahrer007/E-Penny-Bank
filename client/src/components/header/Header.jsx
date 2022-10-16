a4066d8d33154961dcc12d91e851de191a5f56b9
tahrer007 <tahrer_Ad@outlook.com>
2022-10-16 23:08:25 +0300

    navigation home

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import "./header.scss";
 import SvgLogo from "components/header/Svg/SvgLogo";
 import SvgHome from "components/header/Svg/SvgHome";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBuildingColumns } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
+
 
 function Header() {
   const navigate = useNavigate();
@@ -14,7 +13,7 @@ function Header() {
         <SvgLogo />
       </div>
       <h2>Deposits history</h2>
-      <div className="homeBtn" onClick={() => navigate("./home")}>
+      <div className="homeBtn" onClick={() => navigate("/welcome")}>
         <SvgHome />
       </div>
     </div>
