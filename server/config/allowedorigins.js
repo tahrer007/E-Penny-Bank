1bddc6f60917033ca722ec03e66e580c708a7241
tahrer007 <tahrer_Ad@outlook.com>
2022-10-23 03:55:29 +0300

    add cros orgin

--- server/config/allowedorigins.js
+++ server/config/allowedorigins.js
@@ -2,5 +2,6 @@ const allowedOrigins = [
   //"front end link" ,
   "http://127.0.0.1:3000",
   "http://localhost:3000",
+  "https://e-penny-bank.netlify.app/"
 ];
 module.exports =  allowedOrigins ; 
